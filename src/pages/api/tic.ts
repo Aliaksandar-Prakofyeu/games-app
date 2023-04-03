57e8e143f4c3f33c007222ff5fa7c44b54d688e0
Brusnicka <yerbamaniac@gmail.com>
2023-04-04 00:35:03 +0400

    fix: tic tac toe fix

--- src/pages/api/tic.ts
+++ src/pages/api/tic.ts
@@ -1,88 +1,88 @@
-import { NextApiRequest } from "next";
-import { Server } from "socket.io";
+import {NextApiRequest} from 'next'
+import {Server} from 'socket.io'
 
-let currentPlayers: string[] = [];
+let currentPlayers: string[] = []
 
 export default async function SocketHandler(req: NextApiRequest, res: any) {
-  if (res.socket.server.io) {
-    console.log("Already set up");
-    res.end();
-    return;
-  }
+    if (res.socket.server.io) {
+        console.log('Already set up')
+        res.end()
+        return
+    }
 
-  const io = new Server(res.socket.server, {
-    allowEIO3: true,
-  });
+    const io = new Server(res.socket.server, {
+        allowEIO3: true,
+    })
 
-  io.on("connection", (socket) => {
-    socket.on("join_tic_room", (obj) => {
-      console.log("Joined room");
+    io.on('connection', (socket) => {
+        socket.on('join_tic_room', (obj) => {
+            console.log('Joined room')
 
-      socket.join(obj.roomId);
+            socket.join(obj.roomId)
 
-      const count = io.sockets.adapter.rooms.get(obj.roomId)?.size!;
+            const count = io.sockets.adapter.rooms.get(obj.roomId)?.size!
 
-      const isUserWonPreviousGame = obj?.preWinner === obj.username;
+            const isUserWonPreviousGame = obj?.preWinner === obj.username
 
-      const preWinnerAsX = isUserWonPreviousGame ? "X" : "O";
-      const newGameFirstPlayerAsX = count <= 1 ? "X" : "O";
+            const preWinnerAsX = isUserWonPreviousGame ? 'X' : 'O'
+            const newGameFirstPlayerAsX = count <= 1 ? 'X' : 'O'
 
-      const isPreviousPlayer = obj.currentPlayers.includes(obj.username);
+            const isPreviousPlayer = obj.currentPlayers.includes(obj.username)
 
-      const newGame = count > 2 ? "LIVE" : count === 2 ? "START" : "WAIT";
-      const previousGame = isPreviousPlayer ? "START" : "LIVE";
+            const newGame = count > 2 ? 'LIVE' : count === 2 ? 'START' : 'WAIT'
+            const previousGame = isPreviousPlayer ? 'START' : 'LIVE'
 
-      const newObject = {
-        ...obj,
-        playerSymbol: obj.isGameReset ? preWinnerAsX : newGameFirstPlayerAsX,
-        gameStatus: obj.isGameReset ? previousGame : newGame,
-      };
+            const newObject = {
+                ...obj,
+                playerSymbol: obj.isGameReset ? preWinnerAsX : newGameFirstPlayerAsX,
+                gameStatus: obj.isGameReset ? previousGame : newGame,
+            }
 
-      if (currentPlayers.length <= 2) {
-        currentPlayers.push(obj.username);
-      }
+            if (currentPlayers.length <= 2) {
+                currentPlayers.push(obj.username)
+            }
 
-      io.to(socket.id).emit("start_tic", newObject);
+            io.to(socket.id).emit('start_tic', newObject)
 
-      if (count <= 2 && !obj.isGameReset) {
-        io.in(obj.roomId).emit("waiting_lobby", newObject);
-      }
-    });
+            if (count <= 2 && !obj.isGameReset) {
+                io.in(obj.roomId).emit('waiting_lobby', newObject)
+            }
+        })
 
-    socket.on("send_message_tic", (obj) => {
-      io.in(obj.roomId).emit("receive_message_tic", obj);
-    });
+        socket.on('send_message_tic', (obj) => {
+            io.in(obj.roomId).emit('receive_message_tic', obj)
+        })
 
-    socket.on("winner_tic", (obj) => {
-      const result = {
-        result: obj,
-        currentPlayers,
-      };
+        socket.on('winner_tic', (obj) => {
+            const result = {
+                result: obj,
+                currentPlayers,
+            }
 
-      io.in(obj.roomId).emit("announce_winner", result);
-    });
+            io.in(obj.roomId).emit('announce_winner', result)
+        })
 
-    socket.on("leave_room", (obj) => {
-      const count = io.sockets.adapter.rooms.get(obj.roomId)?.size!;
+        socket.on('leave_room', (obj) => {
+            const count = io.sockets.adapter.rooms.get(obj.roomId)?.size!
 
-      if (count === 2) {
-        io.socketsLeave(obj.roomId);
-      } else {
-        socket.leave(obj.roomId);
-      }
+            if (count === 2) {
+                io.socketsLeave(obj.roomId)
+            } else {
+                socket.leave(obj.roomId)
+            }
 
-      io.to(socket.id).emit("reset_tile", obj);
-    });
-  });
+            io.to(socket.id).emit('reset_tile', obj)
+        })
+    })
 
-  res.socket.server.io = io;
+    res.socket.server.io = io
 
-  console.log("Setting up socket");
-  res.end();
+    console.log('Setting up socket')
+    res.end()
 }
 
 export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+    api: {
+        bodyParser: false,
+    },
+}
